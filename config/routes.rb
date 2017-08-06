ce0c02f8c0e2a6184c17b9ee82d032866b42ce9a
Kristel Salvione <kristel.salv@gmail.com>
2017-08-05 23:18:57 -0600

    5.1 added routes to contact, about, products, rerouted landing page

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,16 @@ Rails.application.routes.draw do
   get 'static_pages/landing_page'
 
   resources :products
-  get 'static_pages/about'
+  #get 'static_pages/about'
+  get '/products', to: 'products#index'
 
   get 'static_pages/contact'
 
-  root 'static_pages#landing_page'
-  get 'static_pages/index'
+  #root 'static_pages#landing_page' <--TEMPORARILY REROUTED
+  #get 'static_pages/index' <--TEMPORARILY REROUTED
+
+  root 'static_pages#about' #5.1 TEMPORARILY REROUTED TO ABOUT PAGE
+  get 'static_pages/about' #5.1 TEMPORARILY REROUTED TO ABOUT PAGE
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
