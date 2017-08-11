acb40c319d29211d2616fb46d914c4b0673fa231
Kristel Salvione <kristel.salv@gmail.com>
2017-08-10 22:14:37 -0600

    4.5 removed and updated code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.limit(3)
+    @products = @products = Product.all
   end
 
   # GET /products/1
