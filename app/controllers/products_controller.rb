b41869cd490d985b4b24f69d5bb3775de50a25c0
Kristel Salvione <kristel.salv@gmail.com>
2017-08-12 21:25:47 -0600

    5.2 styled forms Bootstrap

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,12 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = @products = Product.all
-    search_term = params[:q]
+    if params[:q]
+      search_term = params[:q]
+      @products = Product.search(search_term)
+    else
+      @products = @products = Product.all
+    end
   end
 
   # GET /products/1
