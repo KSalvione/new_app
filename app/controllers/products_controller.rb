4877ef2365e981c02f6334487954ed5da81576f5
Kristel Salvione <kristel.salv@gmail.com>
2017-08-12 18:47:29 -0600

    5.2 added a missing line

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = @products = Product.all
     search_term = params[:q]
   end
 
