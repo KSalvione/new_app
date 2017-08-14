eb82c29d50752cced2fcfe5a68d3ed6a40ab3dd5
Kristel Salvione <kristel.salv@gmail.com>
2017-08-14 17:57:49 -0600

    5.3 added contact form and thank you page

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   root 'static_pages#about' #5.1 TEMPORARILY REROUTED TO ABOUT PAGE
   get 'static_pages/about' #5.1 TEMPORARILY REROUTED TO ABOUT PAGE
 
+  post 'static_pages/thank_you'
+
   resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
