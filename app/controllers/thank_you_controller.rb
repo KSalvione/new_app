16f67bbe80c934eea62765b3f7647e5901bb94d6
Kristel Salvione <kristel.salv@gmail.com>
2017-08-15 16:30:42 -0600

    5.3 Added contact form updates, mailers

--- app/controllers/thank_you_controller.rb
+++ app/controllers/thank_you_controller.rb
@@ -3,10 +3,11 @@ class ThankYouController < ApplicationController
 		@name = params[:name]
 		@email = params[:email]
 		@message = params[:message]
-		ActionMailer::Base.mail(from: @email, 
-			to: 'kristel.salv@gmail.com',
-			subject: "A new contact form message from #{@name}",
-			body: @message).deliver_now
+		#ActionMailer::Base.mail(from: @email, 
+		#	to: 'kristel.salv@gmail.com',
+		#	subject: "A new contact form message from #{@name}",
+		#	body: @message).deliver_now
+		UserMailer.contact_form(@email, @name, @message).deliver_now
 	end
 end
 
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+	default from: 'kristel.salv@gmail.com'
+
+	def contact_form(email, name, mesage)
+		@message = message
+		mail(from: email,
+			to: 'kristel.salv@gmail.com',
+			subject: "A new contact form message from #{name}")
+	end
+end
