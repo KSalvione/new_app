5f2e5c6a96bfbcac20adfcf0b731d31b1e0b4e0c
Kristel Salvione <kristel.salv@gmail.com>
2017-09-12 23:22:17 -0600

    5.7 users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-<<<<<<< HEAD
   before_action :authenticate_user!, except: [:show, :index]#to make sure user is signed in first
-=======
-  before_action :authenticate_user!#to make sure user is signed in first
->>>>>>> master
   load_and_authorize_resource#ensures user is authorized to perform action
 
   # GET /users
@@ -77,4 +73,4 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:first_name, :last_name)
     end
-end
+end 
