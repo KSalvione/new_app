67f67574a12ea437455bf5e8547086da87e14fbc
Kristel Salvione <kristel.salv@gmail.com>
2017-09-19 00:02:32 -0600

    5.7 updated product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,11 @@ class Product < ApplicationRecord
 		like_operator = Rails.env.production? ? "ilike" :"LIKE"
 		Product.where("name #{like_operator} ?", "%#{search_term}%")
 		has_many :comments
+		has_many :orders
 	end
 
 	def highest_rating_comment
-		Comment.rating_desc.first
+		Comment.rating_desc.first #capitalized because lowercase C produced an error
 	end
 end
 
