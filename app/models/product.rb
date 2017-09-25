ceea1c4684ad2e5ee8ca063ac446490d600ef95f
Kristel Salvione <kristel.salv@gmail.com>
2017-09-24 21:11:10 -0600

    5.8 changed product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
+	has_many :comments
+	has_many :orders
 	def self.search(search_term)
 		like_operator = Rails.env.production? ? "ilike" :"LIKE"
 		Product.where("name #{like_operator} ?", "%#{search_term}%")
-		has_many :comments
-		has_many :orders
 	end
 
 	def highest_rating_comment
