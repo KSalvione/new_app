e0e587f936a7a7f1a3a28250b46c376efff721c1
Kristel Salvione <kristel.salv@gmail.com>
2017-11-02 12:25:09 -0600

    5.11 added user mailer for registration

---
+++ app/controllers/user_registrations_controller.rb
@@ -0,0 +1,11 @@
+class UserRegistrationsController < Devise::RegistrationsController
+	def create
+		super
+		if @user.persisted?
+			UserMailer.welcome_mail(@user).deliver_now
+		end
+	end
+
+	def edit
+	end
+end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,9 @@ class UserMailer < ApplicationMailer
 			:to => 'kristel.salv@gmail.com',
 			:subject => "A new contact form message from #{name}")
 	end
+
+	def welcome(user)
+		@appname="BB Blades"
+		mail(to: user.email, subject: "Welcome to #{@appname}!")
+	end
 end
