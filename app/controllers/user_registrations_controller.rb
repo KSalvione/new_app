94f9f1de93cb6346150f310a76c1d920ecd38688
Kristel Salvione <kristel.salv@gmail.com>
2017-11-05 21:38:08 -0700

    5.11 changed mailer

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -2,7 +2,7 @@ class UserRegistrationsController < Devise::RegistrationsController
 	def create
 		super
 		if @user.persisted?
-			UserMailer.welcome_mail(@user).deliver_now
+			UserMailer.welcome(@user).deliver_now
 			redirect_to sign_in_path
 		end
 	end
