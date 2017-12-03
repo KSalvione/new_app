333a10908f34646da98eba3582e0dec36c4a456f
Kristel Salvione <kristel.salv@gmail.com>
2017-12-02 23:36:41 -0700

    6.13 actioncable updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
     config.assets.unknown_asset_fallback = true
+
+  config.web_socket_server_url = "wss://bb-blades.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://bb-blades.herokuapp.com', 'http://bb-blades.herokuapp.com']
 end
