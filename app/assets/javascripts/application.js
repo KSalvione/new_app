ca2560c40c33a1a128d3232ae86a063f14ae2db1
Kristel Salvione <kristel.salv@gmail.com>
2017-12-04 20:48:39 -0700

    6.13 changed application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
 //= require jquery3
 //= require jquery_ujs
+//= require rails-ujs
+//= require turbolinks
 //= require_tree .
 //= require jquery.raty
 //= require jquery.elevateZoom-3.0.8.min.js
