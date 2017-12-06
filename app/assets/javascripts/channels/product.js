11b07862edcd0ecf3f2d5efe02697b684868acc3
Kristel Salvione <kristel.salv@gmail.com>
2017-12-05 21:52:42 -0700

    6.14 changed product.js

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -21,9 +21,8 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     });
   }
 
-  $(document).on('turbolinks:load', function() {
+});
+
+$(document).on('turbolinks:load', function() {
     App.product.listen_to_comments();
   });
-
-
-});
