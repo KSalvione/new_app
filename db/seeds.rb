698e578c8e9a70ec71212ed567ababba9877c3e9
Kristel Salvione <kristel.salv@gmail.com>
2018-01-15 21:50:32 -0700

    4.6 added orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.create(name: "seed-generated product", description: "I also created this without using the HTML form!")
+
+Order.create(user_id: 1, product_id: 1, total: 10.0)
+Order.create(user_id: 1, product_id: 2, total: 5.0)
